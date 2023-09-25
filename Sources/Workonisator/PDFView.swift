0bfe5f02f1fa30945fa258269f9b25902d5f43fe
einsaetze-finanzen <einsaetze_finanzen.0r@icloud.com>
2023-09-25 17:44:47 +0200

    Error fixing

--- Sources/Workonisator/PDFView.swift
+++ Sources/Workonisator/PDFView.swift
@@ -4,36 +4,41 @@
 //
 //  Created by Paul Brendtner on 20.09.23.
 //
-//
-//import SwiftUI
-//import PDFKit
-//
-//struct PDFViewerView: View {
-//    let pdfURL: String
-//
-//    init(pdfURL: String) {
-//        self.pdfURL = pdfURL
-//    }
-//
-//    var body: some View {
-//        PDFViewRepresentedView(pdfPath: pdfURL)
-//    }
-//}
-//
-//
-//
-//struct PDFViewRepresentedView: NSViewRepresentable {
-//    let pdfPath: String
-//
-//    func makeNSView(context: Context) -> PDFView {
-//        return PDFView()
-//    }
-//
-//    func updateNSView(_ uiView: PDFView, context: Context) {
-//        if let pdfURL = Bundle.main.url(forResource: pdfPath, withExtension: "pdf") {
-//                if let pdfDocument = PDFDocument(url: pdfURL) {
-//                    uiView.document = pdfDocument
-//                }
-//            }
-//    }
-//}
+
+import SwiftUI
+import PDFKit
+
+#if canImport(UIKit)
+import UIKit
+
+struct PDFViewerView: View {
+    let pdfURL: String
+
+    init(pdfURL: String) {
+        self.pdfURL = pdfURL
+    }
+
+    var body: some View {
+        PDFViewRepresentedView(pdfPath: pdfURL)
+    }
+}
+
+
+
+struct PDFViewRepresentedView: NSViewRepresentable {
+    let pdfPath: String
+
+    func makeNSView(context: Context) -> PDFView {
+        return PDFView()
+    }
+
+    func updateNSView(_ uiView: PDFView, context: Context) {
+        if let pdfURL = Bundle.main.url(forResource: pdfPath, withExtension: "pdf") {
+                if let pdfDocument = PDFDocument(url: pdfURL) {
+                    uiView.document = pdfDocument
+                }
+            }
+    }
+}
+
+#endif
